f50e6e5dcd6a55dd39ea45e8ac867073862aa7d5
DANIEL HENRIQUE LOPES OLIVEIRA <daniel.lopes@embraer.com.br>
2026-09-24 13:52:45 -0300

    Libera CORS para Render

--- fastapi_app.py
+++ fastapi_app.py
@@ -27,14 +27,7 @@ from agent import AgentConfigurationError, AgentRemoteError, create_agent
 app = FastAPI(title="Portal NC User API", version="0.1.0", docs_url="/docs")
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://127.0.0.1:4173",
-        "http://localhost:4173",
-        "http://127.0.0.1:5173",
-        "http://localhost:5173",
-        "http://127.0.0.1:8000",
-        "http://localhost:8000",
-    ],
+    allow_origins=["*"],
     allow_credentials=False,
     allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["Authorization", "Content-Type"],
